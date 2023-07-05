c122c2e0e177aca3af704c5af949dc9a2bae0607
Delhi ganesh <118669075+Delhiganesh04@users.noreply.github.com>
2023-07-05 20:21:25 +0530

    App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React, { useState } from 'react';
+import './App.css';
+import LoginForm from './components/LoginForm';
+import RegistrationForm from './components/RegistrationForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const App = () => {
+  const [isRegistration, setIsRegistration] = useState(true);
+
+  const handleSwitchForm = () => {
+    setIsRegistration(!isRegistration);
+  };
+
+  return (
+    <div className="App">
+      {isRegistration ? (
+        <RegistrationForm handleSwitchForm={handleSwitchForm} />
+      ) : (
+        <LoginForm handleSwitchForm={handleSwitchForm} />
+      )}
+    </div>
+  );
+};
+
+export default App;
