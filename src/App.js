580ef9a3c859ff04b19f956c02b240621b3f9a5a
Delhi ganesh <118669075+Delhiganesh04@users.noreply.github.com>
2023-07-05 20:21:56 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 import LoginForm from './components/LoginForm';
 import RegistrationForm from './components/RegistrationForm';
 
 const App = () => {
+  const [isRegistration, setIsRegistration] = useState(true);
+
+  const handleSwitchForm = () => {
+    setIsRegistration(!isRegistration);
+  };
+
   return (
-    <div>
-      <LoginForm />
-      <RegistrationForm />
+    <div className="App">
+      {isRegistration ? (
+        <RegistrationForm handleSwitchForm={handleSwitchForm} />
+      ) : (
+        <LoginForm handleSwitchForm={handleSwitchForm} />
+      )}
     </div>
   );
 };
