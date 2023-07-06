fc7d617204d0e09020a087adc71b5a51da9db80e
Delhi ganesh <118669075+Delhiganesh04@users.noreply.github.com>
2023-07-06 06:10:13 +0530

    LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
 import './form-styles.css';
-//import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import firebase from '../firebase'; // Update the path based on your file structure
-
-
-
 
 const LoginForm = ({ handleSwitchForm }) => {
   const [email, setEmail] = useState('');
@@ -22,35 +18,54 @@ const LoginForm = ({ handleSwitchForm }) => {
   const handleLogin = (e) => {
     e.preventDefault();
     // Check email and password against registered credentials
-    if(email === 'registered_email@example.com' && password === 'password123') {
+    if (email === 'registered_email@example.com' && password === 'password123') {
       console.log('Login failed');
       // Perform additional actions upon successful login
     } else {
-      console.log('Login successfull');
+      console.log('Login successful');
       // Display error message or perform other actions for failed login
     }
-     // fire base in login
+    // Firebase login
     firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // User login successful
-      const user = userCredential.user;
-      console.log('User login successful:', user);
-      // You can perform additional actions here, like redirecting the user to a dashboard page
-    })
-    .catch((error) => {
-      // User login failed
-      const errorMessage = error.message;
-      console.error('User login failed:', errorMessage);
-      // Display error message or perform other actions for failed login
-    });
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // User login successful
+        const user = userCredential.user;
+        console.log('User login successful:', user);
+        // You can perform additional actions here, like redirecting the user to a dashboard page
+      })
+      .catch((error) => {
+        // User login failed
+        const errorMessage = error.message;
+        console.error('User login failed:', errorMessage);
+        // Display error message or perform other actions for failed login
+      });
+  };
+
+  const handleResetPassword = (e) => {
+    e.preventDefault();
+    // Firebase password reset
+    firebase
+      .auth()
+      .sendPasswordResetEmail(email)
+      .then(() => {
+        console.log('Password reset email sent');
+        // You can display a success message or perform additional actions here
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.error('Password reset email could not be sent:', errorMessage);
+        // Display error message or perform other actions for password reset failure
+      });
   };
 
   return (
-    <form className="add "onSubmit={handleLogin}>
+    <form className="add" onSubmit={handleLogin}>
       <h2>Login</h2>
-      <label className="box" htmlFor="email">Email:</label>
+      <label className="box" htmlFor="email">
+        Email:
+      </label>
       <input
         type="email"
         id="email"
@@ -71,6 +86,9 @@ const LoginForm = ({ handleSwitchForm }) => {
       <br />
 
       <button type="submit">Login</button>
+      <button type="button" onClick={handleResetPassword}>
+        Reset Password
+      </button>
       <div className="reg">
         Don't have an account?{' '}
         <button type="button" className="log" onClick={handleSwitchForm}>
