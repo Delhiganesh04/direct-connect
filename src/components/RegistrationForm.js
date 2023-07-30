813ba9525c8ea6ab9265af81b37d39dc2e22132a
Delhi ganesh <118669075+Delhiganesh04@users.noreply.github.com>
2023-07-30 08:40:19 +0530

    RegistrationForm.js

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -2,11 +2,29 @@ import React, { useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import Select from 'react-select';
+import './form-styles.css';
+import './RegistrationForm.css';
+
+const countryCodes = [
+  { value: '+1', label: '+1 (United States)' },
+  { value: '+44', label: '+44 (United Kingdom)' },
+  { value: '+91', label: '+91 (India)' },
+  { value: '+86', label: '+86 (China)' },
+  { value: '+81', label: '+81 (Japan)' },
+  { value: '+82', label: '+82 (South Korea)' },
+  { value: '+49', label: '+49 (Germany)' },
+  { value: '+33', label: '+33 (France)' },
+  { value: '+39', label: '+39 (Italy)' },
+  { value: '+7', label: '+7 (Russia)' },
+  // Add more country codes as needed
+];
 
 const RegistrationForm = ({ handleSwitchForm }) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [countryCode, setCountryCode] = useState(null);
   const [age, setAge] = useState('');
   const [gender, setGender] = useState('');
   const [qualification, setQualification] = useState('');
@@ -25,6 +43,10 @@ const RegistrationForm = ({ handleSwitchForm }) => {
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
+  };
+  
+  const handleCountryCodeChange = (selectedOption) => {
+    setCountryCode(selectedOption);
   };
 
   const handlePhoneNumberChange = (e) => {
@@ -79,14 +101,17 @@ const RegistrationForm = ({ handleSwitchForm }) => {
     console.log('Confirm Password:', confirmPassword);
 
     try {
-      // Request OTP verification
-      const recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
-      const phoneProvider = new firebase.auth.PhoneAuthProvider();
-      const verificationId = await phoneProvider.verifyPhoneNumber(
-        phoneNumber,
-        recaptchaVerifier
-      );
-      setVerificationId(verificationId);
+      if (countryCode && phoneNumber) {
+        const fullPhoneNumber = `${countryCode.value}${phoneNumber}`;
+        const recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+        const phoneProvider = new firebase.auth.PhoneAuthProvider();
+        const verificationId = await phoneProvider.verifyPhoneNumber(fullPhoneNumber, recaptchaVerifier);
+        setVerificationId(verificationId);
+
+      console.log("OTP sent to respective email");
+      } else {
+        console.error("the user didn't select a country code or didn't enter a phone number");
+      }
     } catch (error) {
       const errorCode = error.code;
       const errorMessage = error.message;
@@ -135,147 +160,185 @@ const RegistrationForm = ({ handleSwitchForm }) => {
   return (
     <form onSubmit={handleSubmit}>
       <h2>Registration</h2>
-      <label htmlFor="name">Name:</label>
+      <div className="boxes">
+      <label htmlFor="name">Name:<span className="required">*</span></label>
       <input
         type="text"
         id="name"
         value={name}
         onChange={handleNameChange}
+        placeholder="Enter your name"
         required
       />
+      </div>
       <br />
-
-      <label htmlFor="email">Email:</label>
+      <div className="boxes">
+      <label htmlFor="email">Email:<span className="required">*</span></label>
       <input
         type="email"
         id="email"
         value={email}
         onChange={handleEmailChange}
+        placeholder="Enter your email"
         required
       />
+      </div>
       <br />
+      <div className="ph">Phone Number:<span className="required">*</span></div>
+      <div className="country-code-phone-container">
+        <div className="country-code-container">
+          <label htmlFor="countryCode"></label>
+          <Select
+            options={countryCodes}
+            value={countryCode}
+            onChange={handleCountryCodeChange}
+            placeholder="+91"
+            isSearchable={false}
+            required
+          />
+        </div>
+        <div className="phone-number-container">
+          <label htmlFor="phoneNumber"></label>
+          <input
+            type="tel"
+            id="phoneNumber"
+            value={phoneNumber}
+            onChange={handlePhoneNumberChange}
+            placeholder="Phone Number"
+            required
+          />
+        </div>
+      </div>
 
-      <label htmlFor="phoneNumber">Phone Number:</label>
-      <input
-        type="tel"
-        id="phoneNumber"
-        value={phoneNumber}
-        onChange={handlePhoneNumberChange}
-        required
-      />
       <br />
-
+      <div className="boxes">
       <label htmlFor="age">Age:</label>
       <input
         type="number"
         id="age"
         value={age}
         onChange={handleAgeChange}
+        placeholder="Enter your age"
         required
       />
+      </div>
       <br />
-
-      <label htmlFor="gender">Gender:</label>
+      <div className="gen">
+      <label htmlFor="gender">Gender:<span className="required">*</span></label>
       <select id="gender" value={gender} onChange={handleGenderChange} required>
         <option value="">Select</option>
         <option value="male">Male</option>
         <option value="female">Female</option>
         <option value="other">Other</option>
       </select>
+      </div>
       <br />
-      
-      <label htmlFor="qualification">Education Qualification:</label>
+
+      <div className="boxes">
+      <label htmlFor="qualification">Education Qualification:<span className="required">*</span></label>
       <input
         type="text"
         id="qualification"
         value={qualification}
         onChange={handleQualificationChange}
+        placeholder="Enter your qualification"
         required
       />
+      </div>
       <br />
-
+      <div className="boxes">
       <label htmlFor="organization">Organization:</label>
       <input
         type="text"
         id="organization"
         value={organization}
         onChange={handleOrganizationChange}
+        placeholder="Enter your organization"
         required
       />
+      </div>
       <br />
-
+      <div className="boxes">
       <label htmlFor="designation">Designation:</label>
       <input
         type="text"
         id="designation"
         value={designation}
         onChange={handleDesignationChange}
+        placeholder="Enter your desiganation"
         required
       />
+      </div>
       <br />
-
-      <label htmlFor="linkedinId">LinkedIn ID:</label>
+      <div className="boxes">
+      <label htmlFor="linkedinId">LinkedIn ID:<span className="required">*</span></label>
       <input
         type="text"
         id="linkedinId"
         value={linkedinId}
         onChange={handleLinkedinIdChange}
+        placeholder="Enter your linkedinId"
         required
       />
+      </div>
       <br />
-
-      <label htmlFor="password">Password:</label>
+      <div className="boxes">
+      <label htmlFor="password">Password:<span className="required">*</span></label>
       <input
         type="password"
         id="password"
         value={password}
         onChange={handlePasswordChange}
+        placeholder="password"
         required
       />
+      </div>
       <br />
-
-      <label htmlFor="confirmPassword">Confirm Password:</label>
+      <div className="boxes">
+      <label htmlFor="confirmPassword">Confirm Password:<span className="required">*</span></label>
       <input
         type="password"
         id="confirmPassword"
         value={confirmPassword}
         onChange={handleConfirmPasswordChange}
+        placeholder="confirm password"
         required
       />
+      </div>
       <br />
 
       <div id="recaptcha-container"></div>
       {verificationId ? (
-            <>
-              <label htmlFor="otp">Enter OTP:</label>
-              <input
-                type="text"
-                id="otp"
-                value={otp}
-                onChange={(e) => setOtp(e.target.value)}
-                required
-              />
-              <br />
-              <button type="button" onClick={handleOTPVerification}>
-                Verify OTP
-              </button>
-            </>
-          ) : (
-            <button type="submit">Register</button>
-          )}
+        <>
+          <label htmlFor="otp">Enter OTP:</label>
+          <input
+            type="text"
+            id="otp"
+            value={otp}
+            onChange={(e) => setOtp(e.target.value)}
+            required
+          />
+          <br />
+          <button type="button" onClick={handleOTPVerification}>
+            Verify OTP
+          </button>
+        </>
+      ) : (
+        <button type="submit">Register</button>
+      )}
 
-          {registrationStatus === 'success' && <p>Registration is successful!</p>}
-          {registrationStatus === 'failed' && <p>Registration failed. Please try again.</p>}
+      {registrationStatus === 'success' && <p>Registration is successful!</p>}
+      {registrationStatus === 'failed' && <p>Registration failed. Please try again.</p>}
 
-          <div className="reg">
-            Already have an account?{' '}
-            <button type="button" className="log" onClick={handleSwitchForm}>
-              Login
-            </button>
-          </div>
-        </form>
-      );
-    };
+      <div className="reg">
+        Already have an account?{' '}
+        <button type="button" className="log" onClick={handleSwitchForm}>
+          Login
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default RegistrationForm;
 
